13679e62c282752b40dd570c10d0c8a7e18416a3
Eoin O'Reilly <eoinoreilly30@gmail.com>
2019-10-13 03:48:19 +0100

    f

--- api/app/db.py
+++ api/app/db.py
@@ -1,11 +1,62 @@
-from elasticsearch import Elasticsearch
 import logging
+import sqlite3
+from sqlite3 import Error
 
-es = Elasticsearch()
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        logging.error(e)
+ 
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(create_table_sql)
+    except Error as e:
+        logging.error(e)
+
+
+def init_database():
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS users (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+ 
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id text PRIMARY KEY,
+                                    access_token text NOT NULL,
+                                    refresh_token text NOT NULL,
+                                    email text NOT NULL,
+                                    scheduling_type text NOT NULL,
+                                    playing_time_by_day text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+ 
+    # create a database connection
+    conn = create_connection(database)
+ 
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+ 
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
 
 
 def add_user(user_id, access_token, refresh_token, email, scheduling_type):
     # TODO: check if user exists
+
+    db_connection = create_connection()
 
     id = user_id + '-' + email
     doc = {
@@ -20,3 +71,5 @@ def add_user(user_id, access_token, refresh_token, email, scheduling_type):
     response = es.index(index='users', doc_type="user", id=id, body=doc)
 
     logging.info('Added user ' + id)
+
+    db_connection.close()
--- api/build/lib.linux-x86_64-2.7/app/config.py
+++
@@ -1,7 +0,0 @@
-client_id = '1f6e2e118d6b4c7196a3df1ae43ada70'
-client_secret = '5df275094d9b464c9f4ada63d1df0c43'
-redirect_uri = 'http://localhost:5000/callback'
-scope = 'user-read-playback-state user-read-email'
-player_polling_time_seconds = 5
-email = 'yourweekinmusic@gmail.com'
-email_password = '-a3sbQFvWz2Y7x^zsskg'
